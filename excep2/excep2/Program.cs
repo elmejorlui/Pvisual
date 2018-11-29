109ce3ab153b55bb2ae80f4df06593ed5a4e3fe7
elmejorlui <44349210+elmejorlui@users.noreply.github.com>
2018-11-28 19:35:51 -0600

    Add files via upload
    segund2 parcial

---
+++ excep2/excep2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excep2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExceptionSample();
+            }
+            catch (excepcion inEx)
+            {
+                Console.WriteLine($"Message:{inEx.Message} sourse: {inEx.Source}  inner: {inEx.InnerException}");
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine($"Message:{fEx.Message} sourse: {fEx.Source}  inner: {fEx.InnerException}");
+                Console.ReadLine();
+            }
+
+        }
+
+        public static void ExceptionSample()
+        {
+            Console.WriteLine($"Mostrar Error de insercion (true o false)");
+            var isErrorInsert = bool.Parse(Console.ReadLine());
+            if (isErrorInsert)
+                throw new excepcion("Error al insertar");
+
+            Console.WriteLine($"Mostrar Error al obtener (true o false)");
+            var isErrorGet = bool.Parse(Console.ReadLine());
+            if (isErrorGet)
+                throw new excepcion("No se encontraron registros para mostrar");
+        }
+    }   
+}
---
+++ threads/threads/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Threads
+{
+    public class hilos
+    {
+        static readonly object firstLock = new object();
+        static readonly object secondLock = new object();
+        static Stopwatch contador1;
+        static Stopwatch contador2;
+
+
+        static void ThreadJob()
+        {
+            contador1 = Stopwatch.StartNew();
+            Console.WriteLine("\t\t\t\tLocking firstLock");
+            lock (firstLock)
+            {
+                Console.WriteLine("\t\t\t\tLocked firstLock");
+                // Wait until we're fairly sure the first thread
+                // has grabbed secondLock
+                Thread.Sleep(1000);
+                Console.WriteLine("\t\t\t\tLocking secondLock");
+                lock (secondLock)
+                {
+                    Console.WriteLine("\t\t\t\tLocked secondLock");
+                }
+                Console.WriteLine("\t\t\t\tReleased secondLock");
+            }
+            Console.WriteLine("\t\t\t\tReleased firstLock");
+            Console.WriteLine("\t\t\t\thilo1: " + contador1.ElapsedMilliseconds);
+        }
+
+        static void Main()
+        {
+            contador2 = Stopwatch.StartNew();
+            new Thread(new ThreadStart(ThreadJob)).Start();
+            // Wait until we're fairly sure the other thread
+            // has grabbed firstLock
+            Thread.Sleep(1050);
+            Console.WriteLine("Locking secondLock");
+            lock (secondLock)
+            {
+                Console.WriteLine("Locked secondLock");
+                Console.WriteLine("Locking firstLock");
+                lock (firstLock)
+                {
+                    Console.WriteLine("Locked firstLock");
+                }
+                Console.WriteLine("Released firstLock");
+            }
+            Console.WriteLine("Released secondLock");
+            Console.WriteLine("hilo2: " + contador2.ElapsedMilliseconds);
+            Console.Read();
+        }
+    }
+}
